50cc0da18e8ba4673d1675fe2bc7263796700cf4
German Mesa <german.mesa@sap.com>
2019-08-08 15:43:46 +0200

    Test

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
-var debugThrowError = false;
-if (debugThrowError) {
-	// throwing an error at any startup script will cause Travis to fail
-	throw new Error();
-	//
-}
+var supertest = require("supertest");
+var should = require("should");
+
+// This agent refers to PORT where program is runninng.
+var server = supertest.agent("http://localhost:" + port);
+
+// UNIT test begin
+describe("SAMPLE unit test",function(){
+  // #1 should return home page
+  it("should return home page",function(done){
+    // calling home page api
+    server
+    .get("/")
+    .expect("Content-type",/json/)
+    .expect(200) // THis is HTTP response
+    .end(function(err,res){
+      // HTTP status should be 200
+      res.status.should.equal(200);
+      // Error key should be false.
+      res.body.error.should.equal(false);
+      done();
+    });
+  });
+});
